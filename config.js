534e3549a23981b746b6aaf13eb4a461b62416e4
死神未満 <34259833+RoyShen12@users.noreply.github.com>
2023-12-01 05:19:55 +0800

    update

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ module.exports = {
   MAX_RETRY: 5,
   JPEG_MAX_MEM: 1536,
   remoteServer: [
-    { ip: '192.168.50.59', threads: 11 }, // mac
-    { ip: '192.168.50.136', threads: 15 }, // PC
-    { ip: '192.168.50.80', threads: 7 }, // nas
+    { ip: '192.168.50.59', threads: 19 }, // mac
+    { ip: '192.168.50.136', threads: 24 }, // PC
+    { ip: '192.168.50.80', threads: 10 }, // nas
     { ip: '192.168.50.98', threads: 3 }, // little nas
   ],
 }
