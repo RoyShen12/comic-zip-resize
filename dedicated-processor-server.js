92a5b2f2daca1adacdf266be913983e4a0d0fc34
死神未満 <34259833+RoyShen12@users.noreply.github.com>
2023-12-01 00:17:04 +0800

    update

--- dedicated-processor-server.js
+++ dedicated-processor-server.js
@@ -2,6 +2,8 @@ const rpc = require('axon-rpc')
 const axon = require('axon')
 const rep = axon.socket('rep')
 
+const chalk = require('chalk')
+
 const Jimp = require('jimp')
 
 const cachedJpegDecoder = Jimp.decoders['image/jpeg']
@@ -21,10 +23,11 @@ server.expose('resize', async (imgBuffer, fn) => {
   index++
   try {
     const buffer = Buffer.from(imgBuffer.data)
+    const inputSize = buffer.byteLength
     console.log(
-      `[${index}] received resize.buf ${(buffer.byteLength / 1e3).toFixed(
-        1
-      )} KB`
+      chalk.whiteBright(
+        `[${index}] received resize.buf ${(inputSize / 1e3).toFixed(1)} KB`
+      )
     )
 
     const jimpInst = await Jimp.read(buffer)
@@ -34,15 +37,20 @@ server.expose('resize', async (imgBuffer, fn) => {
       .quality(80)
       .getBufferAsync(Jimp.MIME_JPEG)
 
+    const resultSize = resultBuffer.byteLength
+    const ratio = ((resultSize / inputSize) * 100).toFixed(1) + '%'
+
     console.log(
-      `[${index}] resultBuffer resized finish ${(
-        resultBuffer.byteLength / 1e3
-      ).toFixed(1)} KB`
+      chalk.greenBright(
+        `[${index}] resized finish ${(resultSize / 1e3).toFixed(
+          1
+        )} KB (${ratio})`
+      )
     )
 
     fn(null, resultBuffer)
   } catch (error) {
-    console.log(`[${index}] throw error`)
+    console.log(chalk.redBright(`[${index}] throw error`))
     fn(error)
   }
 })
