7228d614c8a93d03b2728560fbc3e3afd71a0252
死神未満 <34259833+RoyShen12@users.noreply.github.com>
2023-12-01 02:42:08 +0800

    update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   remoteServer: [
-    { ip: '192.168.50.59', threads: 11 },
-    { ip: '192.168.50.136', threads: 15 },
-    { ip: '192.168.50.80', threads: 7 },
+    { ip: '192.168.50.59', threads: 11 }, // mac
+    { ip: '192.168.50.136', threads: 15 }, // PC
+    { ip: '192.168.50.80', threads: 7 }, // nas
   ],
 }
