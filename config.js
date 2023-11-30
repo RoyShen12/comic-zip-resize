8bcd35f100768d0d36462d61f29e9f41921648be
死神未満 <34259833+RoyShen12@users.noreply.github.com>
2023-12-01 03:38:41 +0800

    update

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ module.exports = {
     { ip: '192.168.50.59', threads: 11 }, // mac
     { ip: '192.168.50.136', threads: 15 }, // PC
     { ip: '192.168.50.80', threads: 7 }, // nas
+    { ip: '192.168.50.98', threads: 3 }, // little nas
   ],
 }
